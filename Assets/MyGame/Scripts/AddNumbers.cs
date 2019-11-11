05a97e57c2404a02ceb7755afed2ad29e2bc8562
ahoerack03 <anna.hoerack2003@gmail.com>
2019-11-11 09:55:19 +0100

    delete lines which we don't need

--- Assets/MyGame/Scripts/AddNumbers.cs
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class AddNumbers : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
     void Start()
     {
         int a = 3;
@@ -18,9 +17,4 @@ public class AddNumbers : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
